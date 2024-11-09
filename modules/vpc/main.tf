e2225433468897e7fd9e782ba575ba8ed21c4c49
unknown <trieuvi27.le@gmail.com>
2024-11-09 17:41:18 +0700

    modify deploy.yaml

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,4 +1,6 @@
 resource "aws_vpc" "main_vpc" {
+  # checkov:skip=CKV2_AWS_12
+  # checkov:skip=CKV2_AWS_11
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
